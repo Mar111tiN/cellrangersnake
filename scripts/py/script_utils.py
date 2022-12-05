b7bee4507afe7dcdb32ecd3af5db4ffe9e55d13f
Martin Szyska <martin.szyska37@gmail.com>
2022-12-05 16:24:21 +0100

    clean file paths

--- scripts/py/script_utils.py
+++ scripts/py/script_utils.py
@@ -30,7 +30,7 @@ def show_output(text, color="normal", multi=False, time=False, **kwargs):
     time = (
         f"\033{colors['time']}{dt.now().strftime('%H:%M:%S')}\033[0m : " if time else ""
     )
-    proc = f"\033{colors['process']}Process {os.getpid()}\033[0m : " if multi else ""
+    proc = f"\033{colors['process']}Process {os.pid()}\033[0m : " if multi else ""
     text = f"\033{colors[color]}{text}\033[0m"
     print(time + proc + text, **kwargs)
 
@@ -52,61 +52,6 @@ def full_path(path, base_folder=os.environ['HOME']):
     if path[0] in ["/", "."]:
         return path
     return os.path.join(base_folder,path)
-
-
-def load_config(config_file="", *, config_path="", **kwargs):
-    '''
-    passes configs to inner functions
-    directly passed arguments overwrite config
-    '''
-    
-    # build the config_file path from config_file and config_path arguments
-    if config_path and not config_file.startswith("/"):
-        config_file = full_path(os.path.join(config_path, config_file))
-
-    if not os.path.splitext(config_file)[-1] in [".yml", "yaml"]:
-        config_file = config_file + ".yml"
-
-    # savely load the config file into config dict
-    try:
-        config = load_config_file(config_file)
-    except:
-        show_output(f"config file {config_file} could not be loaded", color="warning")
-        return {}
-
-    # build base_path relative to HOME path
-    path_config = config['paths']
-    path_config['base_path'] = full_path(path_config['base_path'])
-    # build other paths relative to base_path
-    for path in ['data', 'output', 'info', 'img', 'tables']:
-        key = f"{path}_path"
-        if key in path_config:
-            path_config[key] = full_path(path_config[key], base_folder=path_config['base_path'])
-
-    # load in the kwargs to overwrite config
-    config.update(kwargs)
-    show_output(f"config file {config_file} successfully loaded", color="success")
-    # build the output paths
-    pc = config['paths']
-    for folder in ['output_path', 'img_path', 'tables_path']:
-        if folder in pc:
-            if not os.path.isdir(pc[folder]):
-                show_output(f"Creating folder {pc[folder].replace(pc['base_path'], '')} in base folder")
-                os.makedirs(pc[folder])
-
-    # add external code bases
-    if (code_base_list := pc.get('code_core', "")):
-        # convert code_base to list if only one string is given
-        if isinstance(code_base_list, str):
-            code_base_list = [code_base_list]
-        for code_base in code_base_list:
-            if code_base.endswith('/R'):
-                continue
-            code_base = full_path(code_base)
-            sys.path.append(code_base)
-            show_output(f"Added {code_base} to python path for imports")
-
-    return config
 
 
 def get_path(path, file_type="file", config={}):
